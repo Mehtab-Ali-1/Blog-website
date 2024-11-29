e68bf11bf48164efe53b9eae160d82db9e9b568f
Mehtab Ali <160261202+Mehtab-Ali-1@users.noreply.github.com>
2024-11-29 17:43:51 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-// import Navbar from "../components/nav";
-// import { ThemeProvider } from "next-themes";
-// import Footer from "@/components/footer";
-
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,10 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-         <Navbar/> 
        {children}
-         <Footer/> 
-        
         </body>
     </html>
   );
